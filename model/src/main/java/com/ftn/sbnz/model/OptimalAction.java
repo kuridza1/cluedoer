3b3cf739e4e7415612c397c39ab5bcd89e4b7b22
Trifun Derenj <trifun.derenj@gmail.com>
2026-05-16 01:07:34 +0200

    [WIP] backwards chaining

--- model/src/main/java/com/ftn/sbnz/model/OptimalAction.java
+++ model/src/main/java/com/ftn/sbnz/model/OptimalAction.java
@@ -1,12 +1,17 @@
-// OptimalAction.java
 package com.ftn.sbnz.model;
 
 public class OptimalAction {
     private String action;  // "SUGGESTION", "SHOW", "ACCUSE", "DEFAULT"
-    private Card card;
+    private Card card;      // Za SUGGESTION, SHOW, DEFAULT
     private CardType category;
     private String reasoning;
 
+    // Za ACCUSE - cela kombinacija
+    private Card suspect;
+    private Card weapon;
+    private Card room;
+
+    // Konstruktor za SUGGESTION, SHOW, DEFAULT
     public OptimalAction(String action, Card card, CardType category, String reasoning) {
         this.action = action;
         this.card = card;
@@ -14,14 +19,29 @@ public class OptimalAction {
         this.reasoning = reasoning;
     }
 
+    // Konstruktor za ACCUSE
+    public OptimalAction(String action, Card suspect, Card weapon, Card room, String reasoning) {
+        this.action = action;
+        this.suspect = suspect;
+        this.weapon = weapon;
+        this.room = room;
+        this.reasoning = reasoning;
+    }
+
+    // Getteri
     public String getAction() { return action; }
     public Card getCard() { return card; }
     public CardType getCategory() { return category; }
     public String getReasoning() { return reasoning; }
+    public Card getSuspect() { return suspect; }
+    public Card getWeapon() { return weapon; }
+    public Card getRoom() { return room; }
 
     @Override
     public String toString() {
+        if ("ACCUSE".equals(action)) {
+            return "OptimalAction[ACCUSE: " + suspect.getName() + ", " + weapon.getName() + ", " + room.getName() + "]";
+        }
         return "OptimalAction[" + action + " -> " + card.getName() + "]";
     }
 }
-
