8bbc9616d976a08e7f717778dcbf0135733bcce7
Trifun Derenj <trifun.derenj@gmail.com>
2026-06-16 21:02:53 +0200

    [FIX] Better bluffing rules

--- model/src/main/java/com/ftn/sbnz/model/KnowsCard.java
+++ model/src/main/java/com/ftn/sbnz/model/KnowsCard.java
@@ -1,21 +1,37 @@
 package com.ftn.sbnz.model;
 
-import java.util.Date;
-
 public class KnowsCard {
     private Player player;
     private Card card;
-    private Date timestamp;
+    private long timestamp;
 
     public KnowsCard(Player player, Card card) {
         this.player = player;
         this.card = card;
-        this.timestamp = new Date();
+        this.timestamp = System.currentTimeMillis();
     }
 
-    public Player getPlayer() { return player; }
-    public Card getCard() { return card; }
-    public Date getTimestamp() { return timestamp; }
+    public KnowsCard(Player player, Card card, long timestamp) {
+        this.player = player;
+        this.card = card;
+        this.timestamp = timestamp;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public Card getCard() {
+        return card;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
 
     @Override
     public String toString() {
